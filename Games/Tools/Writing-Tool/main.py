e84a8c738e948d34e94fd07c4a3a05040d9ed23f
Aurelio <aurelioxxx@hotmail.com>
2016-06-15 12:37:58 +0200

    Entries are now saved.

--- Games/Tools/Writing-Tool/main.py
+++ Games/Tools/Writing-Tool/main.py
@@ -1,11 +1,13 @@
-from tkinter import Tk, Canvas, Menu, Toplevel, Text, Scrollbar, Listbox
+from tkinter import Tk, Canvas, Menu, Toplevel, Text, Scrollbar, Listbox, StringVar
 from tkinter.ttk import Frame, Label, Entry, Button, Style
 
 class Main(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
         self.parent = parent
+        self.info = {}
         self.parent.geometry("1400x900+150+150")
+        self.fields = []
         self.init_ui()
 
     def init_ui(self):
@@ -17,24 +19,33 @@ class Main(Frame):
         menubar = Menu(self.parent)
         self.parent.config(menu=menubar)
         menubar.add_command(label="New", command=self.onNew)
-        menubar.add_command(label="Load")
+        menubar.add_command(label="Show Info", command=self.get_info)
         menubar.add_command(label="Exit", command=self.quit)
 
         canvas = Canvas(self, background="white")
         canvas.pack(fill="both", expand=1)
 
     def onNew(self):
-        new = Node(self)
-        labels = new.insert_entry_field("Labels")
-        labels2 = new.insert_entry_field("Labels2")
+        new = Node(self, "Node_entry")
+        label = new.insert_entry_field("Labels")
+        label2 = new.insert_entry_field("Labels2")
         text = new.insert_text_field("Text")
         new.ok_cancel_buttons()
 
+    def get_info(self):
+        for i in self.info:
+            for entry in self.info[i]["Entry"]:
+                print(self.info[i]["Entry"][entry])
+            for text in self.info[i]["Text"]:
+                print(self.info[i]["Text"][text])
+
 class Node(Toplevel):
     """ This class is a catchall for all popup windows."""
-    def __init__(self, parent):
+    def __init__(self, parent, name):
         Toplevel.__init__(self)
         self.parent = parent
+        self.name = name
+        self.entries = {"Entry":{}, "Text":{}}
         self.resizable(0,0)
         self.frame = Frame(self)
         self.init_ui()
@@ -53,7 +64,13 @@ class Node(Toplevel):
 
 
     def save(self):
-        pass
+        for i in self.entries["Entry"]:
+            self.entries["Entry"][i] = self.entries["Entry"][i].get()
+        for i in self.entries["Text"]:
+            self.entries["Text"][i] = self.entries["Text"][i].get("1.0", "end-1c")
+
+        self.parent.info[self.name] = self.entries
+        self.destroy()
 
     def insert_entry_field(self, txt):
         frame = Frame(self.frame)
@@ -62,8 +79,7 @@ class Node(Toplevel):
         label.pack(side="left", anchor="n", padx=5, pady=5)
         entry = Entry(frame)
         entry.pack(fill="x", padx=5, pady=5, expand=True)
-        entryfield = [frame, label, entry]
-        return entryfield
+        self.entries["Entry"][txt] = entry
 
     def insert_text_field(self, txt):
         frame = Frame(self.frame)
@@ -72,8 +88,7 @@ class Node(Toplevel):
         label.pack(side="left", anchor="n", padx=5, pady=5)
         entry = Text(frame)
         entry.pack(fill="both", pady=5, padx=5, expand=True)
-        textfield = [frame, label, entry]
-        return textfield
+        self.entries["Text"][txt] = entry
 
 def main():
     root = Tk()
