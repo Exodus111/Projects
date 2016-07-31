8cc3327a219a7bde9fa6ed5840bd3894a38f4bc5
Aurelio Aguirre <Aurelio Aguirre>
2016-07-31 21:17:31 +0200

    Code is now documented.

--- Other_Scripts/Comic-page/main.py
+++ Other_Scripts/Comic-page/main.py
@@ -1,3 +1,7 @@
+# WebComic App by Exodus111
+# www.github.com/exodus111
+# Use freely, but credit me.
+
 from flask import Flask, render_template, url_for, redirect
 from path import Path
 from comics import Series
@@ -13,17 +17,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    Our entry point, and Home destination.
+    Thumbs is a dictionary where the keys are url routes and the values image src.
+    Series is an object that handles all the Comic Objects.
+    """
     global series
     return render_template("index.html", thumbs=series.toplayer_thumbs)
 
 @app.route("/folder<int:fid>")
 def pick_folder(fid):
+    """
+    The user has selected a subfolder.
+    `fid` : (int) The Folder ID number.
+    We call our method to get the correct thumbnail dictionary.
+    """
     global series
     thumbs = series.set_active_folder(fid)
     return render_template("index.html", thumbs=thumbs)
 
 @app.route("/issue<int:cid>")
 def set_page(cid):
+    """
+    The user has requested an issue, and we call the method to select his issue.
+    `cid` : (int) The Comic ID number.
+    We redirect to the `get_page` function.
+    """
     global series
     series.set_active_issue(cid)
     return redirect(url_for("get_page", num=0))
@@ -31,6 +50,9 @@ def set_page(cid):
 
 @app.route("/page<int:num>")
 def get_page(num=0):
+    """
+    A simple page turner. Num is the page we are on, it enumerates from 0.
+    """
     global series
     image = series.active_comic.get_image(num)
     return render_template("pages.html", image=image, num=num)
