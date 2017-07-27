791e5ec23afee7708b21ab71e9858e26c39dc7c4
Aurelio <aurelioxxx@hotmail.com>
2017-07-27 17:59:10 +0200

    Working on GUI

--- Games/Story-RPG/New/Test/gui_test/gui_test.py
+++ Games/Story-RPG/New/Test/gui_test/gui_test.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.animation import Animation
 from kivy.properties import *
 
+MAIN_COLOR = (0.435, 0.325, 0.239, 1.) # Light Brown.
+SECONDARY_COLOR = (.12,.12,.12, 1.) # Dark.
+WHITE = (1.,1.,1.,1.)
+
 class Element(Widget):
     name = StringProperty()
-    e_size = ListProperty([0,0])
-    e_pos = ListProperty([0,0])
     e_color = ListProperty([0,0,0,0])
     bordersize = NumericProperty(10)
     text_area = ListProperty()
@@ -21,8 +25,18 @@ class Element(Widget):
         anim = Animation(e_pos=pos, t="out_cubic", duration=.5)
         anim.start(self)
 
+    def create_labels(self, labels):
+        self.area = RelativeLayout(size=self.size, pos=self.pos)
+        self.add_widget(self.area)
+        for l in labels:
+            text = Text(**l) # Fix this order. pos_hint must come after add_widget.
+            self.area.add_widget(text)
 
-class GUI(Widget):
+class Text(Label):
+    e_pos = DictProperty()
+
+
+class GUI(FloatLayout):
     w_size = ListProperty([0,0])
     top_bar_texts =  ListProperty(["First Text Area",
                                    "Second Text Area",
@@ -30,58 +44,42 @@ class GUI(Widget):
     right_panel_text = StringProperty("Right Bar")
     left_panel_text = StringProperty("Left Bar")
 
-    def setup(self, size):
-        self.w_size = size
-        gcolor = (0.435, 0.325, 0.239, 1.)
-
-        # Create Right Panel.
-        rp_size, rp_pos = self.placement_and_size("right panel")
-        rp_text = [{"text":self.right_panel_text, "color":(.12,.12,.12, 1.), "pos":(rp_pos[0]+20, rp_pos[1]+20)}]
-        self.right_panel = self.add_element("Right Panel", rp_size, rp_pos, gcolor, rp_text)
-
-        # Create Left Panel.
-        lp_size, lp_pos = self.placement_and_size("left panel")
-        self.left_panel = self.add_element("Left Panel", lp_size, lp_pos, gcolor)
+    def setup(self):
 
         # Create the Top Bar.
         tb_size, tb_pos = self.placement_and_size("top bar")
-        bar_text = [{"text":self.top_bar_texts[0], "color":(.12,.12,.12, 1.), "pos":(tb_pos[0]+20, tb_pos[1]-40)},
-                    {"text":self.top_bar_texts[1], "color":(.12,.12,.12, 1.), "pos":(tb_pos[0]+400, tb_pos[1]-40)},
-                    {"text":self.top_bar_texts[2], "color":(.12,.12,.12, 1.), "pos":(tb_pos[0]+850, tb_pos[1]-40)}]
-        self.top_bar = self.add_element("Top Bar", tb_size, tb_pos, gcolor, bar_text)
+        bar_text = [{"text":self.top_bar_texts[0], "color":WHITE, "e_pos":{"x":.1, "y":1.}},
+                    {"text":self.top_bar_texts[1], "color":WHITE, "e_pos":{"x":.5, "y":1.}},
+                    {"text":self.top_bar_texts[2], "color":WHITE, "e_pos":{"x":.8, "y":1.}}]
+        self.top_bar = self.add_element("Top Bar", tb_size, tb_pos, MAIN_COLOR, bar_text)
+
+        # Create the Top Panel.
+        tp_size, tp_pos = self.placement_and_size("top panel")
+        self.top_panel = self.add_element("Top Panel", tp_size, tp_pos, MAIN_COLOR)
+
 
         # Adding widgets to widget stack.
-        self.add_widget(self.right_panel)
-        self.add_widget(self.left_panel)
+        #self.add_widget(self.top_panel)
         self.add_widget(self.top_bar)
 
     def placement_and_size(self, location):
-        if location == "right panel":
-            s = ((self.w_size[0]/8)*3.2, (self.w_size[1]/8)*6)
-            p = (self.w_size[0], (self.w_size[1]/8)*1.5)
+        if location == "top panel":
+            s = (.8, .5)
+            p = {"x":.2, "top":1.}
             return s, p
-        elif location == "left panel":
-            s = ((self.w_size[0]/8)*3.2, (self.w_size[1]/8)*6)
-            p = (-s[0], (self.w_size[1]/8)*1.5)
-            return s, p
-        elif location == "top panel":
-            pass
         elif location == "bottom panel":
             pass
         elif location == "top bar":
-            s = (self.w_size[0], 20)
-            p = (0, self.w_size[1]-20)
+            s = (1., .025)
+            p = {"x":0., "top":1.}
             return s, p
 
 
-    def add_element(self, name, size, pos, color=(1.,1.,1.,1.), texts=[]):
-        el = Element()
-        el.e_size = size
-        el.e_pos = pos
+    def add_element(self, name, size, pos, color=(WHITE), texts=[]):
+        el = Element(size_hint=size, pos_hint=pos)
+        el.name = name
         el.e_color = color
-        for t in texts:
-            l = Label(**t)
-            el.add_widget(l)
+        el.create_labels(texts)
         return el
 
     def update(self, dt):
--- Games/Story-RPG/New/Test/gui_test/main.py
+++ Games/Story-RPG/New/Test/gui_test/main.py
@@ -15,13 +15,12 @@ from kivy.uix.widget import Widget
 from gui_test import GUI
 
 class MyGame(Widget):
-    wsize = (xwidth, xheight)
 
     def setup(self):
         self.keyboard = Window.request_keyboard(lambda : None, self)
         self.keyboard.bind(on_key_down=self.keydown)
-        self.gui = GUI()
-        self.gui.setup(self.wsize)
+        self.gui = GUI(size=(xwidth, xheight))
+        self.gui.setup()
         self.add_widget(self.gui)
 
     def update(self, dt):
@@ -29,15 +28,12 @@ class MyGame(Widget):
 
     def keydown(self, *e):
         if e[1][1] == "spacebar":
-            if self.gui.right_panel.e_pos[0] == self.wsize[0]:
-                self.gui.right_panel.move([self.wsize[0]-self.gui.right_panel.e_size[0], None])
+            if self.gui.top_panel.e_pos[1] == self.wsize[1]:
+                self.gui.top_panel.move([None, self.wsize[1]-self.gui.top_panel.e_size[1]])
             else:
-                self.gui.right_panel.move([self.wsize[0], None])
+                self.gui.top_panel.move([None, self.wsize[1]])
         if e[1][1] == "c":
-            if self.gui.left_panel.e_pos[0] != 0:
-                self.gui.left_panel.move([0, None])
-            else:
-                self.gui.left_panel.move([-self.gui.left_panel.e_size[0], None])
+            pass
 
 class MainApp(App):
     def build(self):
